5c4206fc7d7b4f581200857bd69476212cfbaa0c
Daniel Fonseca Corradini Ferrand <danielfcf@protonmail.com>
2022-11-29 22:58:02 -0300

    Added functions to DatabaseTinyDB

--- main.py
+++ main.py
@@ -110,9 +110,12 @@ class MainWindow(QtWidgets.QMainWindow):
             self.handle_paint_request(printer)
 
 
+class DatabaseTinyDB:
+
     def __init__(self):
         self.db = TinyDB('testdb.json')
         self.q = Query()
+
     def add(self, x):
         '''
 
@@ -128,25 +131,36 @@ class MainWindow(QtWidgets.QMainWindow):
         '''
         return self.db.all()
 
-    def find(self, x):
+    def find(self, field, x):
         '''
+        Example usage: print(db.find(['type'], ['apple']))
+        :param field: (str) database 'column' to be searched for
+        :param x: (str) value looked for
+        :return: list[dic{}]
+        '''
+        #return self.db.search(self.q[tuple(field)].one_of(x))
+        print(self.db.search(self.q[tuple(field)].one_of(field)))
+        return []#self.db.search(self.db.table('_default').all())
 
-        :param x:
-        :return:
+    def columnsDistinct(self, x):
         '''
-        return self.db.search(x)
+        Must be changed, as db gets bigger this will slow down like hell. does not take nested lists into account!!
+        Used to get all valid items within a datacolumn
+        Example usage: columnsDistinct('type')
+        :param x: (str) 'column' to search for distinct items
+        :return: list[]
+        '''
+        values = []
+        for i in self.db.search(self.q[x].exists()):
+            if i.get(x) not in values:
+                values.append(i.get(x))
+        return values
 
-    def columnsDistinct(self, x, col):
-        '''
-
-        :param x:
-        :param col:
-        :return:
-        '''
-        values=[]
-        for i in self.db.search(x.exists()):
-            if i.get(col) not in values:
-                values.append(i.get(col))
+    def columnNames(self):
+        values = []
+        for i in self.db.table('_default').all()[0]:
+            if i not in values:
+                values.append(i)
         return values
 
 
